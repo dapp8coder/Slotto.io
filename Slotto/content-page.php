fb591efaad9117f17d836c972aa1c49b52bcd5f2
RoundBearChoi <roundbeargames@gmail.com>
2018-07-23 18:27:52 +0900

    choi
    choi

--- Slotto/content-page.php
+++ Slotto/content-page.php
@@ -1,35 +1,26 @@
 <?php
 /**
- * The template used for displaying page content in page.php
+ * Template part for displaying page content in page.php
  *
- * @package Penscratch 2
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
  */
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="entry-thumbnail">
-			<?php the_post_thumbnail( 'penscratch-2-featured' ); ?>
-		</div>
-	<?php endif; ?>
 	<div class="entry-content">
-		<?php the_content(); ?>
 		<?php
+			the_content();
+
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'penscratch-2' ),
+				'before' => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
-	<footer class="entry-footer">
-		<?php edit_post_link(
-			sprintf(
-				/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'penscratch-2' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
