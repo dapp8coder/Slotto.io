c4621074e9351037958fc8298038abe7196f71fc
RoundBearChoi <roundbeargames@gmail.com>
2018-07-25 13:15:33 +0900

    choi
    choi

--- Slotto_v1/scripts/recentDraws.js
+++ Slotto_v1/scripts/recentDraws.js
@@ -29,7 +29,7 @@ async function getRecentDraws() {
         let all = transfers.result;
         let gen = new Array();
         // @ts-ignore
-        let draws = ""; // "all draws since " + getMemoLimit().memo.substring(9, getMemoLimit().memo.length) + " (UTC)" + "<br><br>";
+        let draws = "all draws since " + getMemoLimit().memo.substring(9, getMemoLimit().memo.length) + " (UTC)" + "<br><br>";
 
         for (let i = 0; i < all.length; i++) {
             // @ts-ignore
@@ -46,8 +46,8 @@ async function getRecentDraws() {
         document.getElementById("recentDraws").innerHTML = draws;
 
         // @ts-ignore
-        let g = getGenerationMin() * 60 * 1000;
-        setTimeout("getRecentDraws()", g);
+        //let g = getGenerationMin() * 60 * 1000;
+        setTimeout("getRecentDraws()", 30000);
     }
 }
 
