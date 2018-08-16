35c39e9b42f84ab6eb0d957b52654df6c6cc28e9
RoundBearChoi <roundbeargames@gmail.com>
2018-08-16 15:07:14 +0900

    choi
    choi

--- Slotto_v54/countdown/startCountdown.js
+++ Slotto_v54/countdown/startCountdown.js
@@ -40,3 +40,28 @@ Date.prototype.getThisFriday = function() {
     var d = this.getThisMonday();
     return new Date(d.setDate(d.getDate() + 4));
 };
+
+function whatDayIsToday() {
+    console.log("");
+    console.log("today is " + getDayOfWeek());
+}
+
+function getDayOfWeek() {
+    var today = new Date();
+    switch (today.getDay()) {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+    }
+}
