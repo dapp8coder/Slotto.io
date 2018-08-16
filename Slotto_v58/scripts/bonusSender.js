690c8fdafef27d76082a1aaf53c615d64ea913f2
RoundBearChoi <roundbeargames@gmail.com>
2018-08-16 18:32:55 +0900

    choi
    choi

--- Slotto_v58/scripts/bonusSender.js
+++ Slotto_v58/scripts/bonusSender.js
@@ -28,15 +28,8 @@ async function updateBonuses() {
     let watcher = new Watcher();
     watcher.getWinners(receiveTransfers.result);
 
-    let outstandingStr = "";
-
-    console.log(watcher.outstandingTickets);
-
-    for (let i = 0; i < watcher.outstandingTickets.length; i++) {
-        outstandingStr += watcher.outstandingTickets[i].op[1].from + " " + watcher.outstandingTickets[i].op[1].memo + " " + watcher.outstandingTickets[i].timestamp + "<br>";
-    }
-
-    document.getElementById("outstandingTickets").innerHTML = outstandingStr;
+    showOutstandingTickets(watcher.outstandingTickets);
+    await sendBonuses(sender);
 
     let d = new Date();
     console.log("");
@@ -46,3 +39,60 @@ async function updateBonuses() {
     console.log("updating again in 3 mins");
     setTimeout("updateBonuses();", 3 * 60 * 1000);
 }
+
+function showOutstandingTickets(outstanding) {
+    let outstandingStr = "";
+    outstandingDataArray = new Array();
+
+    for (let i = 0; i < outstanding.length; i++) {
+        let od = new OutstandingData();
+        od.buyer = outstanding[i].op[1].from;
+        od.ticket = outstanding[i].op[1].memo
+        od.timestamp = outstanding[i].timestamp;
+
+        outstandingDataArray.push(od);
+        outstandingStr += od.buyer + " " + od.ticket + " " + od.timestamp + "<br>";
+    }
+
+    document.getElementById("outstandingTickets").innerHTML = outstandingStr;
+}
+
+async function sendBonuses(sender) {
+    let bonusStr = "";
+
+    // @ts-ignore
+    let sendHistory = new SteemHistory(sender);
+    // @ts-ignore
+    sendHistory.setSearchLimit(getMemoLimit().memo, getMemoLimit().sender, null);
+    await sendHistory.download();
+
+    // @ts-ignore
+    let sendTransfers = new SteemTransfers();
+    sendTransfers.filterTransfers(sender, null, sendHistory.result);
+
+    for (let i = 0; i < sendTransfers.result.length; i++) {
+        if (sendTransfers.result[i].op[1].to != "slotto.gen") {
+            if (sendTransfers.result[i].op[1].memo.includes("bonus")) {
+                console.log();
+                bonusStr += sendTransfers.result[i].op[1].to + " " + sendTransfers.result[i].op[1].amount + " " + sendTransfers.result[i].op[1].memo + "<br>";
+            }
+        }
+    }
+
+    document.getElementById("bonusesSent").innerHTML = bonusStr;
+}
+
+function OutstandingData() {
+    this.buyer = null;
+    this.ticket = null;
+    this.timestamp = null;
+}
+
+function BonusData() {
+    this.amount = null;
+    this.ticket = null;
+    this.timestamp = null;
+}
+
+let outstandingDataArray = null;
+let bonusDataArray = null;
