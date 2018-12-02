abb67e1eb8ffa7a664870f37d6f2d021f6fe3dbd
RoundBearChoi <roundbeargames@gmail.com>
2018-12-02 23:26:30 +0900

    choi
    choi

--- Slotto_v83/scripts/steemNode.js
+++ Slotto_v83/scripts/steemNode.js
@@ -1,6 +1,6 @@
 function useDefaultNode() {
     const url = "https://api.steemit.com";
-    //const url = "https://testnet.steemitdev.com";
+    //const url = "https://api.steemitdev.com";
     steem.api.setOptions({ url: url });
     console.log("using default node: " + url);
 }
